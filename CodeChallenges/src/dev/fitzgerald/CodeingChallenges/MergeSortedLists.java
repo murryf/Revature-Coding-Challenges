e896575e10c82c4d702bca5e067a9370e8d0d7b4
murryf <72839972+murryf@users.noreply.github.com>
2022-06-16 09:08:54 -0700

    Add files via upload

--- CodeChallenges/src/dev/fitzgerald/CodeingChallenges/MergeSortedLists.java
+++ CodeChallenges/src/dev/fitzgerald/CodeingChallenges/MergeSortedLists.java
@@ -1,5 +1,6 @@
 package dev.fitzgerald.CodeingChallenges;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,15 +11,24 @@ public class MergeSortedLists {
 
     }
 
-    public static LinkedList MergeSorted(List[] values){
-
-        if(values.length == 0){
-            return new LinkedList();
-        } else if(values.length == 1){
-            LinkedList<Integer> result = (LinkedList<Integer>) values[0];
+    public static LinkedList<Integer> MergeSorted(List<LinkedList<Integer>> values){
+        LinkedList<Integer> result = new LinkedList<>();
+        int lowest = Integer.MAX_VALUE;
+        int sentinel = 0;
+        if(values.size() == 0){
             return result;
+        } else if(values.size() == 1){
+            return values.get(0);
+        } else {
+            for(int i = 0; i < values.size(); i++){
+                for(int j = 0; j < values.get(i).size(); j++){
+                    result.add(values.get(i).get(j));
+                }
+            }
+            Collections.sort(result);
         }
 
-        return null;
+        return result;
     }
 }
+
